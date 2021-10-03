eba8f19a19c6353e876b8e7ea4e056125972758e
Justin Pickens <justin.pickens@gmx.com>
2021-10-03 02:54:48 -0500

    Added resolvers

--- server/schemata/resolvers.js
+++ server/schemata/resolvers.js
@@ -1,21 +1,68 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 const { User, Book } = require('../models');
 
 const resolvers = {
     Query: {
-        me: () => {},
+        me: (parent, args, context) => {
+            if (context.user) {
+                const user = await User.findById(context.user._id)
+                .populate('book');
+        
+                if (user) return user;
+
+                return {};
+             }
+        
+            throw new AuthenticationError('Not logged in');
+        },
     },
     Mutation: {
         login: async (parent, { email, password }) => {
+            const user = await User.findOne({ $or: [{ username: email }, { email: email }] });
 
+            if (!user) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const correctPw = await user.isCorrectPassword(password);
+      
+            if (!correctPw) {
+              throw new AuthenticationError('Incorrect credentials');
+            }
+      
+            const token = signToken(user);
+      
+            return { token, user };
         },
-        addUser: async (parent, { username, email, password }) => {
-
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+      
+            return { token, user };
         },
-        saveBook: async (parent, { authors, description, title, bookId, image, link }) => {
-
+        saveBook: async (parent, args, context) => {
+            try {
+              const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $addToSet: { savedBooks: args } },
+                { new: true, runValidators: true }
+              );
+              return updatedUser;
+            } catch (err) {
+              console.log(err);
+            }
         },
-        removeBook: async (parent, { bookId }) => {
-            
+        removeBook: async (parent, { bookId }, context) => {
+            const updatedUser = await User.findOneAndUpdate(
+                { _id: context.user._id },
+                { $pull: { savedBooks: { bookId: bookId } } },
+                { new: true }
+              );
+              if (!updatedUser) {
+                console.log("Couldn't find user with this id!" );
+              }
+              return updatedUser;
         }
     }
 };
